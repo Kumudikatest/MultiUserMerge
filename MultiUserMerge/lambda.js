0c0c7ab214caee20b68430958ce343d513520e28
Indunil1992 <indunil@adroitlogic.com>
2019-09-09 11:06:27 +0530

    add s3 R and add some  codes for editor

--- MultiUserMerge/lambda.js
+++ MultiUserMerge/lambda.js
@@ -1,4 +1,31 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
+exports.handler = function (event, context, callback) {
+    s3.getObject({
+        'Bucket': "test.indunil1",
+        'Key': "1"
+    }).promise()
+        .then(data => {
+            console.log("pass data");    
+            console.log(data);           // successful response
+            /*
+            data = {
+                AcceptRanges: "bytes", 
+                ContentLength: 3191, 
+                ContentType: "image/jpeg", 
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"", 
+                LastModified: "<Date Representation>", 
+                Metadata: {}, 
+                TagCount: 2, 
+                VersionId: "null"
+            }
+            */
+        })
+        .catch(err => {
+            console.log("get error");  
+            console.log(err, err.stack); // an error occurred
+        });
+
+    callback(null, { "message": "Successfully executed s3" });
 }
